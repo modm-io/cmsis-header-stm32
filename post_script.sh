e5e3e66412a4e551ffe5d3db0a38940c4744982b
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2019-07-18 18:51:35 +0200

    Refactor update script for multithreading

--- post_script.sh
+++ post_script.sh
@@ -2,11 +2,12 @@
 # YOUR MILEAGE MAY VARY
 
 # convert all files to unix line endings
-find . -type f \( -iname "*.h" \) -print0 | xargs -0 -I header dos2unix header header
+find $1 -type f \( -iname "*.h" \) -print0 | xargs -0 -I header dos2unix header header
+find $1 -type f \( -iname "*.h" \) -print0 | xargs -0 -I header chmod 644 header
 
 # yay, computers
 export LANG=C
 export LC_CTYPE=C
 export LC_ALL=C
 # remove all trailing whitespace from the .h files
-find . -type f \( -iname "*.h" \) -not \( -name .git -prune \) -print0 | xargs -0 sed -i 's/\s*$//'
+find $1 -type f \( -iname "*.h" \) -not \( -name .git -prune \) -print0 | xargs -0 sed -i 's/\s*$//'
--- update.py
+++ update.py
@@ -4,10 +4,12 @@
 import urllib.request
 import zipfile
 import shutil
-import sys
-import re
-import os
+import logging
+import os, re, sys
+
 from pathlib import Path
+from multiprocessing.pool import ThreadPool
+from socket import timeout
 
 # Set the right headers
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
@@ -24,8 +26,9 @@ stm32_families = [
     "h7",
     "wb"
 ]
+readme = Path("README.md").read_text()
 
-def get_local_cube_version(readme, family):
+def get_local_cube_version(family):
     regex = r"Cube{} v(?P<version>[0-9]+\.[0-9]+\.[0-9]+)\)".format(family.upper())
     match = re.search(regex, readme)
     return match.group("version") if match else None
@@ -55,72 +58,88 @@ def remote_is_newer(local, remote):
             return True
     return False
 
-download_remote = ("-d" in sys.argv)
-force_update = ("-f" in sys.argv)
 
-cube_local_version = {}
-header_local_version = {}
-print("Parsing local header versions...")
-# parse the versions directly from the README
-readme = Path("README.md").read_text()
-for family in stm32_families:
+download_remote = "-d" in sys.argv
+force_update = "-f" in sys.argv
+logging.basicConfig(level=logging.DEBUG if "-vv" in sys.argv else logging.INFO)
+
+def get_header_files(family):
+    LOGGER = logging.getLogger(family.upper())
+    cube_furl = "http://www.st.com/en/embedded-software/stm32cube{}.html"
+    header_local_version = None
+    LOGGER.debug("Parsing local header versions...")
+    # parse the versions directly from the README
     # extract local cube version from local readme
-    cube_local_version[family] = get_local_cube_version(readme, family)
-    if not cube_local_version[family]:
-        print("No version match in local Readme for", family)
-        exit(1)
+    cube_local_version = get_local_cube_version(family)
+    if cube_local_version is None:
+        LOGGER.critical("No version match in local Readme")
+        return None
     # extract local header version from local release notes
-    with open("stm32{}xx/Release_Notes.html".format(family), "r", errors="replace") as html:
-        header_local_version[family] = get_header_version(html.read())
-    if not header_local_version[family]:
-        print("No version match in local release notes for", family)
-        exit(1)
+    header_local_version = get_header_version(Path("stm32{}xx/Release_Notes.html".format(family)).read_text(errors="replace"))
+    if header_local_version is None:
+        LOGGER.critical("No version match in local release notes")
+        return None
 
-cube_remote_version = {}
-cube_dl_url = {}
-cube_furl = "http://www.st.com/en/embedded-software/stm32cube{}.html"
-# parse the versions and download links from ST's website
-for family in stm32_families:
-    print("Downloading homepage for '{}'...".format(family))
-    with urllib.request.urlopen(urllib.request.Request(cube_furl.format(family), headers=hdr)) as response:
-        html = response.read().decode("utf-8")
-        # extract remote cube version from website
-        cube_remote_version[family] = get_remote_cube_version(html)
-        if not cube_remote_version[family]:
-            print("No version match in remote html for", family)
-            exit(1)
-        # extract cube download link from website
-        cube_dl_url[family] = get_remote_zip_url(html, family)
-        if not cube_dl_url[family]:
-            print("No zip download link for", family)
-            exit(1)
+    cube_remote_version = None
+    cube_dl_url = None
+    # parse the versions and download links from ST's website
+    LOGGER.debug("Downloading homepage...")
+    dl_count = 10
+    while(dl_count >= 0):
+        try:
+            with urllib.request.urlopen(urllib.request.Request(cube_furl.format(family), headers=hdr), timeout=30) as response:
+                LOGGER.debug("Homepage size: {:0.0f} kB".format(int(response.getheader('Content-Length'))/1e3))
+                html = response.read().decode("utf-8")
+                # extract remote cube version from website
+                cube_remote_version = get_remote_cube_version(html)
+                if cube_remote_version is None:
+                    LOGGER.critical("No version match in remote html")
+                    return None
+                # extract cube download link from website
+                cube_dl_url = get_remote_zip_url(html, family)
+                if cube_dl_url is None:
+                    LOGGER.critical("No zip download link")
+                    return None
+                break
+        except timeout:
+            dl_count -= 1
+            if (dl_count <= 0):
+                LOGGER.critical("Failed to download homepage")
+                return None
 
-# compare all versions and print a status page
-check_header_version = [f for f in stm32_families if remote_is_newer(cube_local_version[f], cube_remote_version[f])]
-if force_update: check_header_version = stm32_families;
+    # compare all versions and print a status page
+    check_header_version = force_update or remote_is_newer(cube_local_version, cube_remote_version)
 
-# intermediate report on cube versions
-for family in stm32_families:
-    status = "{}: Cube   v{} -> v{}\t{}"
-    print(status.format(family.upper(), cube_local_version[family], cube_remote_version[family],
-            "update!" if family in check_header_version else "ok"))
+    # intermediate report on cube versions
+    LOGGER.info("Cube   v{:7} -> v{:7}   {}".format(cube_local_version, cube_remote_version,
+                                               "update!" if check_header_version else "ok"))
+    # Return False = "no changes"
+    if not check_header_version:
+        return (cube_remote_version, None, None)
 
-header_remote_version = {}
-header_remote_date = {}
-# compare local and remote header versions
-for family in check_header_version:
+    header_remote_version = None
+    header_remote_date = None
+    dl_file = "{}.zip".format(family)
     # download cmsis pack into zip file
-    dl_count = 0
     if download_remote:
-        print("Downloading '{}.zip' ...\n{}".format(family, cube_dl_url[family]))
-    while(1):
-        dl_file = "{}.zip".format(family)
-        if download_remote:
-            with urllib.request.urlopen(urllib.request.Request(cube_dl_url[family], headers=hdr)) as response, \
-                                  open(dl_file, "wb") as out_file:
-                shutil.copyfileobj(response, out_file)
-        dl_count += 1
-        print("Extracting '{}.zip' ...".format(family))
+        LOGGER.debug("Downloading {}".format(cube_dl_url))
+        dl_count = 10
+        while(dl_count >= 0):
+            try:
+                with urllib.request.urlopen(urllib.request.Request(cube_dl_url, headers=hdr), timeout=300) as response, \
+                     open(dl_file, "wb") as out_file:
+                    LOGGER.debug("Zipfile size: {:0.1f} MB".format(int(response.getheader('Content-Length'))/1e6))
+                    shutil.copyfileobj(response, out_file)
+                break
+            except timeout:
+                dl_count -= 1
+                if (dl_count <= 0):
+                    LOGGER.critical("Failed to download zipfile")
+                    return None
+
+    LOGGER.debug("Extracting zipfile...")
+    dl_count = 10
+    while(dl_count >= 0):
         # extract the remote header version from the zip file
         try:
             with zipfile.ZipFile(dl_file, "r") as zip_ref:
@@ -128,54 +147,60 @@ for family in check_header_version:
                 release_note_path = "{}/Drivers/CMSIS/Device/ST/STM32{}xx/Release_Notes.html".format(base_name, family.upper())
                 # only read the release notes, we don't care about the rest
                 html = zip_ref.read(release_note_path).decode("utf-8", errors="replace")
-                header_remote_version[family] = get_header_version(html)
-                header_remote_date[family] = get_header_date(html)
+                header_remote_version = get_header_version(html)
+                header_remote_date = get_header_date(html)
             break
         except:
-            if (dl_count > 10):
-                print("===>>> Bad zipfile for {}! <<<===".format(family))
-                header_remote_version[family] = "x.x.x"
-                break
+            if (dl_count <= 0):
+                LOGGER.critical("===>>> Bad zipfile! <<<===")
+                return None
             else:
-                print("<<<=== Retrying zipfile for {}! ===>>>".format(family))
-    if not header_remote_version[family]:
-        print("No version match in remote release notes for", family)
-        exit(1)
+                LOGGER.error("<<<=== Retrying zipfile! ===>>>")
+            dl_count -= 1
+    if header_remote_version is None:
+        LOGGER.critical("No version match in remote release notes")
+        return None
 
-print()
-headers_updated = []
-for family in check_header_version:
-    status = "{}: Header v{} -> v{}\t{}"
-    hl = header_local_version[family]
-    hr = header_remote_version[family]
-    print(status.format(family.upper(), hl, hr, "update!" if remote_is_newer(hl, hr) else "ok"))
+    headers_updated = False
+    LOGGER.info("Header v{:7} -> v{:7}   {}".format(header_local_version, header_remote_version,
+                "update!" if remote_is_newer(header_local_version, header_remote_version) else "ok"))
     try:
-        print("Replacing headers stm32{}xx...".format(family))
+        LOGGER.info("Replacing headers...")
+        destination_path = "stm32{}xx".format(family)
+        for fobj in Path(destination_path).glob("Include/*"):
+            fobj.unlink()
         with zipfile.ZipFile("{}.zip".format(family), "r") as zip_ref:
-            base_name = "{}/Drivers/CMSIS/Device/ST/STM32{}xx/".format(zip_ref.namelist()[0].split("/")[0], family.upper())
-            destination_path = "stm32{}xx".format(family)
+            base_name = "{}/Drivers/CMSIS/Device/ST/STM32{}xx/".format(
+                        zip_ref.namelist()[0].split("/")[0], family.upper())
             sources = ["Release_Notes.html", "Include/s"]
             for member in zip_ref.namelist():
                 if any(member.startswith(os.path.join(base_name, src)) for src in sources):
                     with zip_ref.open(member) as mem, \
-                        open(os.path.join(destination_path, member.replace(base_name, '')), "wb") as dp:
+                         open(os.path.join(destination_path, member.replace(base_name, '')), "wb") as dp:
                         shutil.copyfileobj(mem, dp)
-        headers_updated.append(family)
+        headers_updated = True
     except zipfile.BadZipFile:
-        print("Skipping bad zip file for stm32{}xx...".format(family))
+        LOGGER.error("Skipping bad zip file...")
         pass
 
-print("Normalizing newlines and whitespace...", flush=True)
-if os.system("sh ./post_script.sh > /dev/null 2>&1") != 0:
-    print("Normalizing newlines and whitespace FAILED...")
-    exit(1)
+    LOGGER.debug("Normalizing newlines and whitespace...")
+    if os.system("sh ./post_script.sh stm32{}xx > /dev/null 2>&1".format(family)) != 0:
+        LOGGER.critical("Normalizing newlines and whitespace FAILED...")
+        return None
 
-for family in headers_updated:
     for patch in Path('patches').glob("{}*.patch".format(family)):
-        print("Applying {}...".format(patch))
+        LOGGER.info("Applying {}...".format(patch))
         if os.system("git apply -v --ignore-whitespace {}".format(patch)) != 0:
-            print("Applying {} FAILED...".format(patch))
-            exit(1)
+            LOGGER.critical("Applying {} FAILED...".format(patch))
+            return None
+
+    LOGGER.info("Successful update")
+    return (cube_remote_version, header_remote_version, header_remote_date)
+
+
+with ThreadPool(len(stm32_families)) as pool:
+    family_versions = pool.map(get_header_files, stm32_families)
+
 
 def update_readme(readme, family, new_version, new_date, new_cube):
     match = r"{0}: v.+? created .+? \(Cube{0} v.+?\)".format(family.upper())
@@ -183,9 +208,9 @@ def update_readme(readme, family, new_version, new_date, new_cube):
                     family.upper(), new_version, new_date, new_cube)
     return re.sub(match, replace, readme)
 
-for family in headers_updated:
-    readme = update_readme(readme, family,
-                           header_remote_version[family],
-                           header_remote_date[family],
-                           cube_remote_version[family])
+for family, versions in zip(stm32_families, family_versions):
+    if versions is None or versions[1] is None: continue;
+    readme = update_readme(readme, family, versions[1], versions[2], versions[0])
+
 Path("README.md").write_text(readme)
+exit(family_versions.count(None))
